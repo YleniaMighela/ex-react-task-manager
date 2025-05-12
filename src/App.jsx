24888f75d3f7558e56855a0badd648541e82199f
Ylenia <yleniamighela@gmail.com>
2025-05-12 11:59:06 +0200

    saved API url

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+// mi salvo in una costantae l'URL
+const apiUrl = import.meta.env.VITE_API_URL;
+// console.log('API URL:', import.meta.env.VITE_API_URL);
+
+
 // importo react-router
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 // importo il Layout
 import DefaultLayout from "./layout/DefaultLayout";
@@ -11,11 +17,13 @@ import HomePage from "./pages/HomePage";
 
 
 
+
 function App() {
 
 
   return (
     <>
+
       <BrowserRouter>
         <Routes>
           <Route element={<DefaultLayout />} >
