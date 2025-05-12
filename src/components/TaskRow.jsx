1d2c9f412c5f606214050d7749f96f0f1e203a2d
Ylenia <yleniamighela@gmail.com>
2025-05-12 16:14:35 +0200

    created different style for columns

--- src/components/TaskRow.jsx
+++ src/components/TaskRow.jsx
@@ -2,8 +2,14 @@ export default function TaskRow({ task }) {
 
     return (
         <tr >
-            <td>{task.title}</td>
-            <td>{task.status}</td>
+            <td >{task.title}</td>
+            <td style={{
+                backgroundColor:
+                    task.status === 'To do' ? '#f87171' :
+                        task.status === 'Doing' ? '#facc15' :
+                            task.status === 'Done' ? '#4ade80' : 'white'
+
+            }}>{task.status}</td>
             <td>{new Date(task.createdAt).toLocaleDateString()}</td>
         </tr>
     );
