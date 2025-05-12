14dc6bae5fc4085437e47bbff67cb5ae143fc040
Ylenia <yleniamighela@gmail.com>
2025-05-12 10:36:50 +0200

    fixed HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,17 @@ export default function HomePage() {
 
 
     return (
-        <h1>EX  - Task Manager Avanzato</h1>
+        <div className="home">
+
+            <p>TaskBool è il task manager avanzato pensato per aiutarti a gestire le tue attività in modo intelligente, rapido e organizzato.
+                Crea, modifica, ordina e filtra i tuoi task con facilità grazie a un’interfaccia intuitiva e reattiva.
+                Con la ricerca ottimizzata, i filtri avanzati e le conferme sicure tramite modali, avrai sempre tutto sotto controllo, senza distrazioni.
+
+                Inizia ora a semplificare la tua giornata. Perché ogni task completato è un passo verso i tuoi obiettivi.
+
+            </p>
+        </div>
+
 
     );
 }
