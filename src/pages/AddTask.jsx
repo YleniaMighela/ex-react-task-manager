b3871bf98fc6f0d95ff88500c148d9a54f588ae8
Ylenia <yleniamighela@gmail.com>
2025-05-13 15:56:19 +0200

    added form static

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,10 +1,63 @@
+import { useState, useRef } from 'react'
+
+
+
 export default function AddTask() {
 
+    // campo controllato
+    const [nameTitle, setNameTitle] = useState("");
+
+    //  campo non controllato
+    const descriptionRef = useRef();
+    const statusRef = useRef();
 
     return (
-        <div className="add-task">
-            <h3>Conterrà il form per aggiungere un nuovo task</h3>
-        </div>
+        <>
+            <div className='container_add-task'>
+                <h1>Aggiungi una task</h1>
+
+                <form>
+                    <div className="add-task">
+                        {/* sezione per inserimento nome task */}
+                        <label>
+                            Nome della Task:
+                            <input
+                                type="text"
+                                value={nameTitle}
+                                onChange={(e) => setNameTitle(e.target.value)}
+                                placeholder='Inserisci nome del task'
+                            />
+                        </label>
+
+
+                        {/* sezione per inserimento descrizione */}
+                        <label>
+                            Descrizione:
+                            <textarea
+                                ref={descriptionRef}
+                                placeholder='Inserisci una descrizione'
+                            />
+                        </label>
+
+                        {/* sezione per inserimento stato */}
+                        <label>
+                            Seleziona lo stato
+                            <select
+                                ref={statusRef}
+                            >
+                                <option value="">To do</option>
+                                <option value="Doing">Doing</option>
+                                <option value="Done">Done</option>
+
+                            </select>
+                        </label>
+
+                    </div>
+                </form>
+
+            </div>
+        </>
+
 
     );
 }
