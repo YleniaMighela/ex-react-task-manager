7abaac4b387c4c35d63215a337785c440e4e18a5
Ylenia <yleniamighela@gmail.com>
2025-05-14 12:11:28 +0200

    added button and added comments

--- src/pages/TaskDetail.jsx
+++ src/pages/TaskDetail.jsx
@@ -6,15 +6,23 @@ import { useParams } from "react-router-dom";
 
 export default function TaskDetail() {
 
+    // estrae il parametro id dall'oggetto
     const { id } = useParams();
     const { tasks } = useContext(GlobalContext)
 
+    // cerco con il metodo find il primo elemento che soddisferà la condizione con l'id selezionato
     const task = tasks.find(t => t.id === parseInt(id));
 
+    // se la task non è stata trovata ritornami un "messaggio"
     if (!task) {
         return <p>Task non trovata.</p>;
     }
 
+    // funzione per eliminare la task
+    const handleDelete = () => {
+        console.log("ELimina task");
+
+    }
     return (
 
         <div className='container_add-task'>
@@ -24,7 +32,7 @@ export default function TaskDetail() {
                 <p><strong className="detail_task">Descrizione:</strong>{task.description}</p>
                 <p><strong className="detail_task">Stato:</strong>{task.status}</p>
                 <p><strong className="detail_task">Data di creazione:</strong>{new Date(task.createdAt).toLocaleDateString()}</p>
-
+                <button onClick={handleDelete}>Elimina task</button>
             </div>
 
 
