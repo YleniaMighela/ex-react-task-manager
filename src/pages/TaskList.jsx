d969ce9e321538c43adf3a7f7545b6ec3b9f916b
Ylenia <yleniamighela@gmail.com>
2025-05-15 12:19:57 +0200

    added state, modified table, function handleSort

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,5 +1,5 @@
 // importo il GlobalContext
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import GlobalContext from "../context/GlobalContext";
 import TaskRow from "../components/TaskRow";
 
@@ -7,6 +7,23 @@ export default function TaskList() {
     // destrutturo la prorietà tasks dall'oggeto che ho passata tramite Global
     const { tasks } = useContext(GlobalContext)
 
+    // rappresenta il criterio di ordinmento
+    const [sortBy, setSortBy] = useState('createdAt');
+
+    // rappresenta la direzione
+    const [sortOrder, setSortOrder] = useState(1);
+
+
+
+    // funzione
+    const handleSort = (field) => {
+        if (field === sortBy) {
+            setSortOrder(-sortOrder)
+        } else {
+            setSortBy(field);
+            setSortOrder(1);
+        }
+    }
     return (
         <>
             <h3>Lista dei task</h3>
@@ -16,9 +33,9 @@ export default function TaskList() {
                     <table>
                         <thead>
                             <tr>
-                                <th>Nome</th>
-                                <th>Stato</th>
-                                <th>Data di Creazione</th>
+                                <th onClick={() => handleSort('title')}>Nome</th>
+                                <th onClick={() => handleSort('status')}>Stato</th>
+                                <th onClick={() => handleSort('createdAt')}>Data di Creazione</th>
                             </tr>
                         </thead>
 
