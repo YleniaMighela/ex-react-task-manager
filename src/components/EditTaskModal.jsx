9a9831d388057af554832bdbac767567f19035ab
Ylenia <yleniamighela@gmail.com>
2025-05-15 09:35:51 +0200

    created component with Modal but without form

---
+++ src/components/EditTaskModal.jsx
@@ -0,0 +1,19 @@
+import Modal from "./Modal"
+
+export default function EditTaskModal({ show, onClose, task, onSave }) {
+
+
+
+
+    return (
+
+        <Modal
+            title="Modifica Task"
+            content={ }
+            confirmText="Salva"
+            onConfirm={ }
+        />
+    )
+
+
+};
